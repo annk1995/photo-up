4cd2252619c103f679c9e8811078a119d7193558
annk1995 <kabuikariuki052@gmail.com>
2022-05-28 15:19:25 +0300

    added url and proper routing

---
+++ albumshare/photos/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def gallery(request):
+    return render(request,'gallery.html')
+
+def viewPhoto(request,pk):
+    return render(request,'photo.html')
+
+def addPhoto(request):
+    return render(request,'add.html')
