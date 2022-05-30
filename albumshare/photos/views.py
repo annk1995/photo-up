fe9b18f6fe3c22e8086e5d7b6ac80e6d98e024e1
annk1995 <kabuikariuki052@gmail.com>
2022-05-31 00:41:32 +0300

    made the gallery loop

--- albumshare/photos/views.py
+++ albumshare/photos/views.py
@@ -1,11 +1,17 @@
+from multiprocessing import context
 from django.shortcuts import render
 from .models import Category,Photo
 
 # Create your views here.
 def gallery(request):
     categories =Category.objects.all()
-    return render(request,'gallery.html')
+    photos =Photo.objects.all()
 
+
+    context = {'categories' :categories,'photos' :photos}
+    return render(request,'gallery.html',context)
+    
+    
 def viewPhoto(request,pk):
     return render(request,'photo.html')
 
