fbd9b7d08b7c593acd454611031c2e4c37e38fcf
annk1995 <kabuikariuki052@gmail.com>
2022-05-30 12:05:43 +0300

    added models

--- albumshare/photos/models.py
+++ albumshare/photos/models.py
@@ -1,3 +1,16 @@
+from unicodedata import category
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name=models.CharField(max_length=100,null=False,blank=False)
+
+    def __str__(self):
+        return self.name
+class Photo(models.Model):
+    category =models.ForeignKey(category, on_delete=models.SET_NULL)
+    image= models.ImageField(null=False,blank=False)
+    description =models.CharField(max_length=500 ,null=False ,blank=False)
+
+    def __str__(self):
+        return self.name
