ff66d0fc778c019586caad379c179ca46575954c
annk1995 <kabuikariuki052@gmail.com>
2022-05-30 23:02:50 +0300

    made migrations

--- albumshare/photos/models.py
+++ albumshare/photos/models.py
@@ -8,7 +8,7 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 class Photo(models.Model):
-    category =models.ForeignKey(category, on_delete=models.SET_NULL)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
     image= models.ImageField(null=False,blank=False)
     description =models.CharField(max_length=500 ,null=False ,blank=False)
 
