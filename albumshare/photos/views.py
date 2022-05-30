bf8c847ac501a1df3a1d0765c213e2edc741381e
annk1995 <kabuikariuki052@gmail.com>
2022-05-31 01:14:46 +0300

    added to photo page solved error

--- albumshare/photos/views.py
+++ albumshare/photos/views.py
@@ -12,8 +12,9 @@ def gallery(request):
     return render(request,'gallery.html',context)
     
     
-def viewPhoto(request,pk):
-    return render(request,'photo.html')
+def viewPhoto(request, pk):
+    photo = Photo.objects.get(id=pk)
+    return render(request, 'photo.html', {'photo': photo})
 
 def addPhoto(request):
     return render(request,'add.html')
